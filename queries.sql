66c83be1a1994d8bf90bca0691a548a9bd620332
Tyson Heesacker <tysonheesacker86@gmail.com>
2020-11-19 15:53:47 -0800

    question4

--- queries.sql
+++ queries.sql
@@ -8,4 +8,9 @@ SELECT first_name, last_name, hire_date
 FROM employee
 WHERE hire_date BETWEEN '1986-01-01' and '1986-12-31'
 
-
+-- Dept Manager
+SELECT d.dept_no, d.dept_name, dm.emp_no, first_name, last_name
+FROM department d
+	INNER JOIN dept_manager dm ON d.dept_no = dm.dept_no 
+	INNER JOIN employee e ON e.emp_no = dm.emp_no
+	
