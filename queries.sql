aebdc4c6a6326bcd0c5989706c034f48a6b51d72
Tyson Heesacker <tysonheesacker86@gmail.com>
2020-11-19 16:11:25 -0800

    questions 5,6,7

--- queries.sql
+++ queries.sql
@@ -13,4 +13,30 @@ SELECT d.dept_no, d.dept_name, dm.emp_no, first_name, last_name
 FROM department d
 	INNER JOIN dept_manager dm ON d.dept_no = dm.dept_no 
 	INNER JOIN employee e ON e.emp_no = dm.emp_no
+
+-- Employee Department
+SELECT e.emp_no, first_name, last_name, dept_name
+FROM employee e
+	INNER JOIN department_emp de ON e.emp_no = de.emp_no 
+	INNER JOIN department d ON de.dept_no = d.dept_no
 	
+--Hercules
+SELECT first_name, last_name, gender
+FROM employee
+WHERE first_name = 'Hercules' AND last_name LIKE'B%'
+
+-- Sales Dept
+SELECT e.emp_no, first_name, last_name, dept_name
+FROM employee e
+	INNER JOIN department_emp de ON e.emp_no = de.emp_no 
+	INNER JOIN department d ON de.dept_no = d.dept_no
+	WHERE dept_name = 'Sales'
+	
+-- Sales & Development
+SELECT e.emp_no, first_name, last_name, dept_name
+FROM employee e
+	INNER JOIN department_emp de ON e.emp_no = de.emp_no 
+	INNER JOIN department d ON de.dept_no = d.dept_no
+	WHERE dept_name = 'Sales' OR dept_name = 'Development'
+
+
